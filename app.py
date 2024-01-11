940d559155bcf546cec4af96cca5dea684aecb5e
David Lynch <dlynch@wikimedia.org>
2024-01-11 15:22:00 -0600

    Links to navigate backwards and forwards

--- app.py
+++ app.py
@@ -25,12 +25,12 @@ app.url_map.converters['date'] = DateConverter
 
 
 @app.route('/')
-@app.route('/<date:from_date>:<date:to_date>')
-def dashboard(from_date=False, to_date=False):
+@app.route('/<date:to_date>')
+def dashboard(from_date=False, to_date=False, days=30):
     earliest, latest = database.fetch_date_bounds()
 
-    from_date = from_date or (latest - datetime.timedelta(days=30))
     to_date = to_date or latest  # - datetime.timedelta(days=0)
+    from_date = to_date - datetime.timedelta(days=days)
 
     to_date = min(latest, to_date)
     from_date = max(earliest, from_date)
@@ -41,7 +41,10 @@ def dashboard(from_date=False, to_date=False):
     dates = [(from_date + datetime.timedelta(days=x + 1)).isoformat() for x in range((to_date - from_date).days)]
 
     return render_template('index.html',
-        counts=counts, dates=dates, sites=sites, all=request.args.get('all') == '1')
+        counts=counts, dates=dates, sites=sites, all=request.args.get('all') == '1',
+        earliest=earliest, latest=latest, from_date=from_date, to_date=to_date,
+        forward_date=min(latest, to_date+datetime.timedelta(days=days)),
+    )
 
 
 @app.template_filter('site_activated')
